702b78e4d08c1795d1b1cf0c25a247444db64f6f
Thomas Besluau <cocosinus@hotmail.com>
2015-08-20 00:00:43 +0000

    functional state queueing mules as well

--- dist/harvester.js
+++ dist/harvester.js
@@ -14,11 +14,25 @@ module.exports = function (creep) {
 			}
 		}
 	}*/
+	if (!creep.memory.mule) {
+	    creep.memory.mule = 'incoming';
+	    hatchery.prodQ.push({
+	        role: 'mule',
+	        target: creep.name
+	    });
+	}
 	if (creep.carry.energy < creep.carryCapacity) {
-		var roomSources = creep.room.find(FIND_SOURCES);
-		var source = roomSources[creep.memory.harvestSource];
-		creep.moveTo(source);
-		creep.harvest(source);
+		var source = creep.memory.source;
+		if (!source.assigned || source.assigned === 'incoming') {
+		    source.assigned = creep.name;
+		}
+		var sources = creep.room.find(FIND_SOURCES).filter(function (src) {
+		    return src.id === source.source.id;
+		});
+		creep.moveTo(sources[0]);
+		creep.harvest(sources[0]);
+	} else if (creep.memory.mule && creep.memory.mule !== 'incoming') {
+	    //
 	} else {
 		var spawn = Game.spawns[hatchery.spawnName]
 		creep.moveTo(spawn);
--- dist/hatchery.js
+++ dist/hatchery.js
@@ -42,49 +42,15 @@ module.exports = function (hatcheryName) {
 	if (hatchery.prodQ.length) {
 		var prodObj = hatchery.prodQ[0];
 		var body = Memory.settings.creepRoles[prodObj.role].body;
-		if(spawn.canCreateCreep() == OK) {
-			spawn.createCreep(body, undefined, {role: prodObj.role, hatcheryName: hatcheryName});
+		if(spawn.canCreateCreep(body) == OK) {
+			spawn.createCreep(body, undefined, {
+			    role: prodObj.role,
+			    hatcheryName: hatcheryName,
+			    source: prodObj.source
+			});
 			hatchery.prodQ.shift();
 		}
 	}
-	// the hatchery manages its creeps
-	// spawn.memory.creeps is a list
-	/*var rolesCount = {};
-	var totalCount = 0;
-	for(var index in spawn.memory.creeps) {
-		var creepName = spawn.memory.creeps[index];
-		var creep = Game.creeps[creepName];
-		var role = creep.memory.role;
-		if (!roles[role]) {
-			// Load roles on demand
-			roles[role] = require(role);
-		}
-		// make the creep execute it's role.
-		roles[role](creepName);
-
-		// no mercy for the weak or the old creep
-		if (creep.ticksToLive < 1 || creep.hits < 1) {
-			// bye bye creep
-			spawn.creeps.splice(index, 1);
-		} else {
-			// creep is counted in the stats determining what creeps to make
-			rolesCount[role] = rolesCount[role] ? rolesCount[role] + 1 : 1;
-			totalCount++;
-		}
-	}
-		
-	for(var creepRole in Memory.settings.creepRatio) {
-		var body = Memory.settings.creepBodies[creepRole];
-		var goodValue = Memory.settings.creepRatio[creepRole] / 100 * totalCount;
-		if (
-			(rolesCount[creepRole] || 0) < goodValue + 1 &&
-			spawn.canCreateCreep(body) == OK
-		) {
-			spawn.memory.creeps = spawn.memory.creeps || [];
-			spawn.memory.creeps.push(spawn.createCreep(body, undefined, {role: creepRole, hatcheryName: spawnName}));
-			break;
-		}
-	}*/
 	
 };
 
